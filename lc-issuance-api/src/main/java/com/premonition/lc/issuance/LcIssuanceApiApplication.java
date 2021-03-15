cf4f1ba2d14c53888b639b8fdadacd57954770dd
Prem Chandrasekaran <premanandc@gmail.com>
2021-03-15 10:52:21 -0400

    Added skeleton LC issuance API project

---
+++ lc-issuance-api/src/main/java/com/premonition/lc/issuance/LcIssuanceApiApplication.java
@@ -0,0 +1,13 @@
+package com.premonition.lc.issuance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LcIssuanceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LcIssuanceApiApplication.class, args);
+	}
+
+}
