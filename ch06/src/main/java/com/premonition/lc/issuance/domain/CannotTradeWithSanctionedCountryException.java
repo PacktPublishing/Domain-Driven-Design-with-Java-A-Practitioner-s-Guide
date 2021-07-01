db60217ce6eaf985c2c010c3091a217479d2597d
Premanand Chandrasekaran <premanandc@gmail.com>
2021-07-01 12:49:26 -0400

    Removed unused code
    Co-Authored-By: karthik-krishnan <384746+karthik-krishnan@users.noreply.github.com>

--- ch06/src/main/java/com/premonition/lc/issuance/domain/CannotTradeWithSanctionedCountryException.java
+++
@@ -1,7 +0,0 @@
-package com.premonition.lc.issuance.domain;
-
-public class CannotTradeWithSanctionedCountryException extends DomainException {
-    public CannotTradeWithSanctionedCountryException() {
-        super("Import from this country is currently prohibited!");
-    }
-}
